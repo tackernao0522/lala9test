188fd4c08401c4a19340e08b15df88ebbd946002
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-16 16:30:41 +0900

    22. 外部キーのfactoryの指定方法と挙動の違い

--- tests/Feature/Http/Controllers/PostListControllerTest.php
+++ tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -39,5 +40,19 @@ class PostListControllerTest extends TestCase
             ->assertOk()
             ->assertSee('ブログのタイトル1')
             ->assertSee('ブログのタイトル2');
+    }
+
+    /**
+     * @test
+     */
+    function factoryの観察()
+    {
+        // $post = Post::factory()->make(['user_id' => null]);
+        // dump($post);
+        // dump($post->toArray());
+
+        // dump(User::get()->toArray());
+
+        $this->assertTrue(true);
     }
 }
