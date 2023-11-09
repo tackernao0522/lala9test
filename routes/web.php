677d54a48b8df89c4bd4dc76446f9a83be05abd3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-09 12:00:24 +0900

    41. ログイン画面が開ける

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Http\Controllers\Mypage\UserLoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [PostController::class, 'index']);
 Route::get('posts/{post}', [PostController::class, 'show'])
-->name('posts.show')
-->whereNumber('post'); // 'post'は数値のみに限定という意味
+    ->name('posts.show')
+    ->whereNumber('post'); // 'post'は数値のみに限定という意味
 
 Route::get('signup', [SignupController::class, 'index']);
 Route::post('signup', [SignupController::class, 'store']);
+
+Route::get('mypage/login', [UserLoginController::class, 'index']);
